152543c14d835eda691c315abe5dd63e4f63b3a8
CarolineMorton <carolinemorton@gmx.com>
2021-11-11 17:19:18 +0000

    place holder function added to demonstrate documentation

---
+++ a_module/addition.py
@@ -0,0 +1,14 @@
+""" This is a module that needs to be documented """
+
+
+def add_two(num: int) -> int:
+    """
+    This function takes in a number and returns a number with 2 added
+
+    Args:
+        num: The input number
+
+    Returns:
+        Result
+    """
+    return num + 2
